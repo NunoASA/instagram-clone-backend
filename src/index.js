2e35475c50042f902e12e89be1b7a48d98a2c67a
Nuno Almeida <nunoalmeida@abbeyis.co.uk>
2019-06-15 16:25:39 +0100

    create API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
-const app = express()
-mongoose.connect('mongodb+srv://userFromApp:lvNl31r6tSV1BBkD@instagram-clone-obbbk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-	return res.send(`hello  ${req.query.name}`)
+mongoose.connect('mongodb+srv://userFromApp:lvNl31r6tSV1BBkD@instagram-clone-obbbk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+app.use((req, res) => {
+	req.io = io;
+	next()
 })
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
 
 app.listen(3333)
